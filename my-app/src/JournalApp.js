ee0c17329f920d2fb651f70a2b30c15631611347
aanxhel <ang.core92@yahoo.com>
2021-03-19 20:20:27 -0600

    Update JournalApp.js
    demo con nav barr

--- my-app/src/JournalApp.js
+++ my-app/src/JournalApp.js
@@ -9,31 +9,30 @@ function JournalApp(){
     return(
         <div className='JournalApp'>
 <header id="main-header">
+
             <nav class="navbar 
-            navbar-expand-sm 
+            navbar-expand-md
             bg-dark 
             navbar-dark
-            justify-content-center">
-                
-            <ul class="navbar-nav" >
-                <li class="nav-item">
-                <a class="navbar-brand" href="#">Quienes somos?</a>
-                </li>
-                <li class="nav-item">
-                <a class="navbar-brand" href="#">Productos</a>
-                </li>
-                <li class="nav-item">
-                <a class="navbar-brand" href="#">Fuego</a>
-                </li>
+            justify-content-left">
 
-                <li class="nav-item">
-                <a class="navbar-brand" href="#">Otro Item</a>
-                </li>
 
-            </ul>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+
+  </div>
+     
             </nav>
+
             </header>
+
+
+
+
+            
 
             <div >                                 
                 <img src ={Caballo} class="mx-auto" ></img>
