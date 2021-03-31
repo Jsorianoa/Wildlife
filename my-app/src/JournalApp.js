672121978afcd8791e3f089fcf0620122898e023
Aanxhel <ang.core92@yahoo.com>
2021-03-30 22:03:12 -0600

    correccion de branche

--- my-app/src/JournalApp.js
+++ my-app/src/JournalApp.js
@@ -1,44 +1,32 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./Header.js";
-import Footer from "./Footer"
+import Header from "./comp/Header.js";
+import Footer from "./comp/Footer";
+import InfoText from "./comp/InfoText.js";
+
 
 
 
 function JournalApp() {
   return (
     <div>
-      <Header/>
-      <div class="container">    
 
-          <div>
-            <h2>TITLE HEADING</h2>
-            <h5>Title description, Dec 7, 2017</h5>
-            <div class="fakeimg">Fake Image</div>
-            <p>Some text..</p>
-            <p>
-              Sunt in culpa qui officia deserunt mollit anim id est laborum
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco.
-            </p>
-            <br></br>
-            <h2>TITLE HEADING</h2>
-            <h5>Title description, Sep 2, 2017</h5>
-            <div class="fakeimg">Fake Image</div>
-            <p>Some text..</p>
-            <p>
-              Sunt in culpa qui officia deserunt mollit anim id est laborum
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco.
-            </p>
-          </div>
-        
+      <Header />
+
+      <div class="container">
+
+        <div>
+          <InfoText />
+          <br></br>
+          <InfoText />
+          <br></br>
+          <InfoText />
+
+        </div>
       </div>
-      
-      <Footer/>
-      
+
+      <Footer />
+
     </div>
   );
 }
