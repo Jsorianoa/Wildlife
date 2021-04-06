f17d4e9b0cfc273973c40f693acbd600581187bf
Jorge Antonio Soriano <57119732+Jsorianoa@users.noreply.github.com>
2021-04-05 22:00:00 -0500

    update nombre

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
 
         <div className="jumbotron" id="footerEdit">
-            <p>hola</p>
+            
             <div id="footerIcons">
                 <a href="https://www.facebook.com/wildlifemanagementmx"><i class="fab fa-facebook" ></i></a>
                 <a href="/"><i class="fab fa-twitter"></i></a>
