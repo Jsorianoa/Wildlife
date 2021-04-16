e3381b430a55143d09eca9bfd315e88b4c12510b
Aanxhel <ang.core92@yahoo.com>
2021-04-16 12:36:23 -0500

    Inicio de seccion de gallery

---
+++ my-app/src/components/Gallery.jsx
@@ -0,0 +1,14 @@
+
+const Gallery = ()=>{
+    return(
+        <div className="DivGalleryMain">
+            
+            <h1>Galeria</h1>
+
+            
+
+        </div>
+    )
+}
+
+export default Gallery;
