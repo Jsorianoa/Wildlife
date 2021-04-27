d1150860c8a028a88a3a4aa922aec3251712f196
Jorge Antonio Soriano <57119732+Jsorianoa@users.noreply.github.com>
2021-04-27 01:32:57 -0500

    cambios en el grid

--- my-app/src/components/Gallery.jsx
+++ my-app/src/components/Gallery.jsx
@@ -1,23 +1,22 @@
-import {Container,Row,Col} from 'react-bootstrap'
-
+import wl3 from "./img/wl3.jpg";
 
 function Gallery(){
   return (
     <div className="DivGalleryMain">
       
       <h1 className="TitleGallery">Galeria</h1>
-      <Container>
-        <Row>
-          <Col>1 of 1</Col>
-          <Col>1 of 1</Col>
-        </Row>
 
-        <Row>
-          <Col>1 of 1</Col>
-          <Col>1 of 1</Col>
-        </Row>
-      </Container>
-
+      <div className="container-gallery">
+        <div className="m1"><img  src={wl3} alt="wl3"/></div>
+        <div className="m2"><img  src={wl3} alt="wl3"/></div>
+        <div className="m3"><img  src={wl3} alt="wl3"/></div>
+        <div className="m4"><img  src={wl3} alt="wl3"/></div>
+        <div className="m5"><img  src={wl3} alt="wl3"/></div>
+        <div className="m6"><img  src={wl3} alt="wl3"/></div>
+        <div className="m7"><img  src={wl3} alt="wl3"/></div>
+        <div className="m8"><img  src={wl3} alt="wl3"/></div>
+        <div className="m9"><img  src={wl3} alt="wl3"/></div>
+      </div>
 
     </div>
   );
